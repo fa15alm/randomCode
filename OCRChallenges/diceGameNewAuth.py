4b711afc3502576ab722a6aad7c4b97e6731f0a6
DefiniteError <63715176+DefiniteError@users.noreply.github.com>
2020-09-25 15:25:05 +0100

    Create diceGameNewAuth.py

---
+++ OCRChallenges/diceGameNewAuth.py
@@ -0,0 +1,72 @@
+import random
+import time
+import sys
+import re
+
+def isEven(num):
+    numd = num / 2
+    numid = num // 2
+    if numd == numid:
+        return True
+    else:
+        return False
+
+def write(string):
+    for char in string:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print()
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+    def roll(self, amount):
+        while True:
+            result = random.randint(1, amount)
+            if result == 1:
+                continue
+            else:
+                break
+        self.score += result
+        if isEven(result):
+            self.score += 10
+        else:
+            self.score -= 5
+        return str(result)
+
+# p1name = input('What is your name, Player one? ')
+# write('Hello, ' + p1name)
+# p1 = Player(p1name)
+
+# p2name = input('What is your name, Player two? ')
+# write('Hello, ' + p2name)
+# p2 = Player(p2name)
+
+with open('names.txt', 'r') as nf:
+    re.findall
+
+rtp = input('Ready to play? (Press Enter) ')
+
+for x in range(1, 6):
+    p1roll = p1.roll(12)
+    write(p1.name + ' rolled a total of ' + p1roll)
+    time.sleep(0.5)
+    p2roll = p2.roll(12)
+    write(p2.name + ' rolled a total of ' + p2roll)
+
+print()
+print(p1.name + '\'s score was ' + str(p1.score))
+print(p2.name + '\'s score was ' + str(p2.score))
+print()
+if p1.score > p2.score:
+    print(p1.name + ' won!')
+    with open('champs.txt', 'a') as f:
+        f.write(p1.name + ' won against ' + p2.name + ' with ' + str(p1.score) + ' points!')
+elif p2.score > p1.score:
+    print(p2.name + ' won!')
+    with open('champs.txt', 'a') as f:
+        f.write(p2.name + ' won against ' + p1.name + ' with ' + str(p2.score) + ' points!')
+else:
+    print('It was a draw!')
